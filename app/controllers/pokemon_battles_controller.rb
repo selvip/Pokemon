7ba4014100177ec828d188bb5ec038ad24a63977
selvip <selvipebrina@rocketmail.com>
2016-10-20 17:33:33 +0700

    adding validation

--- app/controllers/pokemon_battles_controller.rb
+++ app/controllers/pokemon_battles_controller.rb
@@ -10,6 +10,7 @@ class PokemonBattlesController < ApplicationController
 
 	def create
 		@pokemon_battle = PokemonBattle.new(pokemon_battle_params)
+		get_each_pokemon
 		set_pokemon_battle_attr
 		if @pokemon_battle.valid?
 			@pokemon_battle.save
@@ -38,22 +39,26 @@ class PokemonBattlesController < ApplicationController
 		attacker = Pokemon.find(params[:attacker_id])
 		skill = PokemonSkill.find(params[:skill_id])
 		if @pokemon_battle.current_turn.odd?
-			if attacker == @pokemon1
+			if @attacker == @pokemon1
+				@defender = @pokemon2
 				skill.current_pp -= 1
 				skill.save
 				@pokemon_battle.current_turn += 1
 				@pokemon_battle.save
 				flash[:danger] = ""
+				check_win
 			else
 				flash[:danger] = "Pokemon 1 turn."
 			end
 		else
 			if attacker == @pokemon2
+				defender = pokemon1
 				skill.current_pp -= 1
 				skill.save
 				@pokemon_battle.current_turn += 1
 				@pokemon_battle.save
 				flash[:danger] = ""
+				check_win
 			else
 				flash[:danger] = "Pokemon 2 turn."
 			end
@@ -62,8 +67,15 @@ class PokemonBattlesController < ApplicationController
 	end
 
 	private
-	
-	
+	def check_win
+		if @defender.current_health_point <= 0
+			@defender.current_health_point = 0
+			@pokemon_battle.state = "Finished"
+			@pokemon_battle.pokemon_winner_id = attacker.id
+			@pokemon_battle.pokemon_loser_id = defender.id
+			@pokemon_battle.save
+		end
+	end
 	def pokemon_battle_params
 		params.require(:pokemon_battle).permit(
 									 :pokemon1_id, 
@@ -76,8 +88,8 @@ class PokemonBattlesController < ApplicationController
 		@pokemon_battle.pokemon_winner_id = 0
 		@pokemon_battle.pokemon_loser_id = 0
 		@pokemon_battle.experience_gain = 0
-		@pokemon_battle.pokemon1_max_health_point = 0
-		@pokemon_battle.pokemon2_max_health_point = 0
+		@pokemon_battle.pokemon1_max_health_point = @pokemon1.max_health_point
+		@pokemon_battle.pokemon2_max_health_point = @pokemon2.max_health_point
 	end
 	def get_each_pokemon
 		@pokemon1 = Pokemon.find(@pokemon_battle.pokemon1_id)
